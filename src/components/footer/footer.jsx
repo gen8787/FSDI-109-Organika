430ff673f4ece0e2b8b40472d3639f20e5d3febc
Gary <66178774+gen8787@users.noreply.github.com>
2021-01-31 10:57:37 -0800

    init commit

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+                <hr/>
+                <center>this is my footer</center>
+            </div>
+        );
+    }
+}
+
+export default Footer;
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'; // imrc
+
+// cc
+class Navbar extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+                <h1>Menu here later</h1>
+                <hr/>
+                <p>Some more text</p>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
